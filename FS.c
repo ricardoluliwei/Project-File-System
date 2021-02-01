5db2179fbdb912e5c9fde6d03b0c955859bf3868
Liwei Lu <ricardoluliwei@gmail.com>
2021-01-31 20:25:32 -0800

    structure finished

--- FS.c
+++ FS.c
@@ -4,9 +4,15 @@
 
 #include "FS.h"
 
-#define NUMBER_OF_BLOCKS 1000  // number of blocks
+#define NUMBER_OF_BLOCKS 64  // number of blocks
 #define BLOCK_SIZE 512
-#define MAX_FILES 10
+#define MAX_FILES 192
 
+// Disk
 unsigned char D[NUMBER_OF_BLOCKS][BLOCK_SIZE];
+
+unsigned char I[BLOCK_SIZE]; // Input buffer
+unsigned char O[BLOCK_SIZE]; // Output buffer
+unsigned char M[BLOCK_SIZE]; // main memory
+struct OFT_entry OFT[BLOCK_SIZE]; // open file table
 
--- FS.h
+++ FS.h
@@ -1,7 +1,12 @@
 #ifndef FS_H
 #define FS_H
 
+#define NUMBER_OF_BLOCKS 1000  // number of blocks
+#define BLOCK_SIZE 512
+#define MAX_FILES 10
 
+
+// User Interface
 void create();
 void destroy();
 int open();
@@ -11,15 +16,31 @@ void write();
 void seek();
 void directory();
 
+// Disk access functions
+void read_block(int b);
+void write_block(int b);
+
+// Initialization
+void init();
+
 struct File_descriptor{
-    int size;
+    int size; // 
     int b0;
     int b1;
     int b2;
 };
 
-struct File_entry{
+// entry in the directory file
+struct Directory_entry{
     char name[4];
+    int fd;
+};
+
+// entry in the open file table
+struct OFT_entry{
+    unsigned char buffer[BLOCK_SIZE];
+    int current_position; // current_position in the file from 0 to 1536
+    int file_size;
     int fd;
 };
 
